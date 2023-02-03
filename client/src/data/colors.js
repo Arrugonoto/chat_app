a9c8af64b491ec959187e4880882f8dd04bbd9f3
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-02-03 13:14:26 +0100

    Added static data for Room personalization - colors
    Planning to add new feature, customization of room. User will be able to change room background color and messages background color with font color.

---
+++ client/src/data/colors.js
@@ -0,0 +1,69 @@
+export const roomBgColor = [
+   { id: 1, value: 'hsl(0, 97%, 44%)' },
+   { id: 2, value: 'hsl(152, 96%, 46%)' },
+   { id: 3, value: 'hsl(283, 75%, 51%)' },
+   { id: 4, value: 'hsl(323, 100%, 50%)' },
+   { id: 5, value: 'hsl(54, 80%, 52%)' },
+   { id: 6, value: 'hsl(187, 100%, 50%)' },
+   { id: 7, value: 'hsl(0, 0%, 85%)' },
+   { id: 8, value: 'hsl(0, 0%, 100%)' },
+];
+export const roomBgGradient = [
+   {
+      id: 1,
+      value: `linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 82%)`,
+   },
+   {
+      id: 2,
+      value: `linear-gradient(0deg, rgba(195,34,195,1) 0%, rgba(45,219,253,1) 82%)`,
+   },
+   {
+      id: 3,
+      value: `linear-gradient(0deg, rgba(208,38,117,1) 7%, rgba(13,215,89,1) 77%)`,
+   },
+   {
+      id: 4,
+      value: `linear-gradient(0deg, rgba(65,204,173,1) 14%, rgba(246,52,162,1) 91%)`,
+   },
+   {
+      id: 5,
+      value: `linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)`,
+   },
+   {
+      id: 6,
+      value: `linear-gradient(180deg, #52ACFF 25%, #FFE32C 100%)`,
+   },
+   {
+      id: 7,
+      value: `linear-gradient(45deg, #FA8BFF 14%, #2BD2FF 51%, #2BFF88 88%)`,
+   },
+   {
+      id: 8,
+      value: `linear-gradient(45deg, #FA8BFF 14%, #2BD2FF 51%, #2BFF88 88%)`,
+   },
+];
+export const messageBgColor = [
+   { id: 1, value: `#0076CE` },
+   { id: 2, value: `#E0115F` },
+   { id: 3, value: `#50C878` },
+   { id: 4, value: `#662d91` },
+   { id: 5, value: `#FEBE10` },
+   { id: 6, value: `#F9629F` },
+];
+export const messageBgGradient = [
+   { id: 1, value: `linear-gradient(90deg, #1CB5E0 0%, #000851 100%)` },
+   { id: 2, value: `linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)` },
+   { id: 3, value: `linear-gradient( 135deg, #3C8CE7 10%, #00EAFF 100%)` },
+   {
+      id: 4,
+      value: `radial-gradient(circle, rgba(21,194,193,1) 8%, rgba(205,22,166,1) 71%)`,
+   },
+   {
+      id: 5,
+      value: `radial-gradient(circle, rgba(21,194,193,1) 8%, rgba(22,205,82,1) 71%)`,
+   },
+   {
+      id: 6,
+      value: `linear-gradient(210deg, #3fdea6 8%, #35d1e6 32%, #ea58e7 59%, #ece549 78%, #ff4747 100%)`,
+   },
+];
