03e1a43842d5b5f2e2f60ce72ef0cc616df5cdb1
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-03-05 14:45:16 +0100

    Refactored fetch function for sending/editing message - deleted comment

--- client/src/components/ChatRoom/MessageForm.js
+++ client/src/components/ChatRoom/MessageForm.js
@@ -36,7 +36,6 @@ const MessageForm = ({ socket }) => {
       setText(e.target.value);
       if (editFlag) setMessageValue(e.target.value);
    };
-   // FIXME:refactor sending message with editing
    const handleSubmit = async e => {
       e.preventDefault();
       const requestMethod = editFlag ? 'PATCH' : 'POST';
