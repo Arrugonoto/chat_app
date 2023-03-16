10aa9e2bea7ac28e15bd1ba193f814430e6125f1
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-03-16 20:12:46 +0100

    Created emoji picker component, moved-emoji mart logic there from messageForm component

--- client/src/components/ChatRoom/EmojiPicker.js
+++ client/src/components/ChatRoom/EmojiPicker.js
@@ -1,20 +1,52 @@
-import { useEffect } from 'react';
+import { useRef } from 'react';
 import { EmojiPickerStyled } from './MessageForm.styled';
+
+// emoji mart component
 import data from '@emoji-mart/data';
 import Picker from '@emoji-mart/react';
 
-const EmojiPicker = ({ setText, displayPicker, setDisplayPicker }) => {
-   const closePicker = () => {};
+// context
+import { useMessageContext } from '../../context/MessagesContext';
 
-   useEffect(() => {
-      // window.addEventListener('keydown', closePicker)
-   }, []);
+// hooks
+import useConditionalListener from '../../hooks/useConditionalListener';
+
+const EmojiPicker = ({
+   setText,
+   displayPicker,
+   setDisplayPicker,
+   pickerBtn,
+}) => {
+   const { editFlag, setMessageValue } = useMessageContext();
+   const pickerContainerRef = useRef(null);
+
+   const handleKeyDown = e => {
+      if (e.key === 'Escape') setDisplayPicker(false);
+   };
+   const handlePointerDown = e => {
+      if (
+         !pickerContainerRef.current.contains(e.target) &&
+         !pickerBtn.current.contains(e.target)
+      )
+         setDisplayPicker(false);
+   };
+
+   const handleAddEmoji = e => {
+      if (!editFlag) {
+         setText(prev => prev + e.native);
+      } else {
+         setMessageValue(prev => prev + e.native);
+      }
+   };
+
+   useConditionalListener('keydown', handleKeyDown, displayPicker);
+   useConditionalListener('pointerdown', handlePointerDown, displayPicker);
 
    return (
-      <EmojiPickerStyled>
+      <EmojiPickerStyled ref={pickerContainerRef}>
          <Picker
             data={data}
-            onEmojiSelect={e => setText(prev => prev + e.native)}
+            onEmojiSelect={handleAddEmoji}
             previewPosition={'top'}
             maxFrequentRows={2}
          />
