5b37d8f48c4b52ecfed138cc01fcdc79fc6daf02
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-04-16 18:44:25 +0200

    update: added static data for user reactions which will be used for reaction buttons

---
+++ client/src/data/reactions.js
@@ -0,0 +1,8 @@
+export const emojiReactions = [
+   { id: 1, name: 'like', value: '\u{1F44D}' },
+   { id: 2, name: 'funny', value: '\u{1F602}' },
+   { id: 3, name: 'wholesome', value: '\u{1F60A}' },
+   { id: 4, name: 'fear', value: '\u{1F631}' },
+   { id: 5, name: 'astonished', value: '\u{1F632}' },
+   { id: 6, name: 'angry', value: '\u{1F621}' },
+];
