74d26c96527597247874fb5eb0165cc80a90521f
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-04-22 21:49:29 +0200

    feat(messageRections): added functionality for displaying number of reactions of message and type of reaction

--- client/src/components/message/messageReactions.js
+++ client/src/components/message/messageReactions.js
@@ -1,7 +1,23 @@
 import { StyledMessageReactions } from './messageReactions.styled';
+import { emojiReactions } from '../../data/reactions';
+import { useThemeContext } from '../../context/ThemeContext';
 
-const MessageReactions = () => {
-   return <StyledMessageReactions></StyledMessageReactions>;
+const MessageReactions = ({ reactions, numOfReactions, msgContainerRef }) => {
+   const { isDarkTheme } = useThemeContext();
+
+   return (
+      <StyledMessageReactions
+         parentWidth={msgContainerRef.current.clientWidth}
+         darkMode={isDarkTheme}
+      >
+         <span>{numOfReactions}</span>
+         {Object.values(reactions)?.map((el, i) =>
+            el.length > 0 ? (
+               <span key={emojiReactions[i].id}>{emojiReactions[i].value}</span>
+            ) : null
+         )}
+      </StyledMessageReactions>
+   );
 };
 
 export default MessageReactions;
